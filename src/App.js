c36f920aea2c3ff89c169e1770c4157ed3d8c26e
Wan <wanshengdev@gmail.com>
2020-10-20 14:40:50 -0700

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import ReactGA from 'react-ga';
-import { createBrowserHistory } from 'history';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ReactGA from "react-ga";
+import { createBrowserHistory } from "history";
 
-import Header from './components/Header/Header';
-import Index from './components/Index/Index';
-import About from './components/About/About';
-import Projects from './components/Projects/Projects';
-import Blogs from './components/Blogs/Blog';
-import PageNotFound from './components/PageNotFound/PageNotFound';
+import Header from "./components/Header/Header";
+import Index from "./components/Index/Index";
+import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
+import Blogs from "./components/Blogs/Blog";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
 
-ReactGA.initialize('UA-176439950-1');
+ReactGA.initialize("UA-176439950-1");
 ReactGA.pageview(window.location.pathname + window.location.search);
 const history = createBrowserHistory();
 history.listen((location) => {
@@ -21,18 +21,18 @@ history.listen((location) => {
 function App() {
 	return (
 		<Router history={history}>
-			<div className='App'>
+			<div className="App">
 				<Header />
-				<div className='container'>
+				<div className="container">
 					<Switch>
-						<Route exact path='/' component={Index} />{' '}
-						<Route exact path='/about' component={About} />{' '}
-						<Route exact path='/projects' component={Projects} />{' '}
-						<Route exact path='/blogs' component={Blogs} />{' '}
-						<Route path='*' component={PageNotFound} />{' '}
-					</Switch>{' '}
-				</div>{' '}
-			</div>{' '}
+						<Route exact path="/" component={Index} />
+						<Route exact path="/about" component={About} />
+						<Route exact path="/projects" component={Projects} />
+						<Route exact path="/blogs" component={Blogs} />
+						<Route path="*" component={PageNotFound} />
+					</Switch>
+				</div>
+			</div>
 		</Router>
 	);
 }
